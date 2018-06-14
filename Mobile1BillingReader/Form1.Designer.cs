11d60af5d259352a8bc251b7bc3fdc391253ed3a
Patrick Kasteel <patrick@mobile1.cloud>
2018-06-14 20:41:40 +0200

    Add project files.

---
+++ Mobile1BillingReader/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Mobile1BillingReader
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.BtnLoad = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNrOfInvoices = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblProcessed = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblBillable = new System.Windows.Forms.Label();
+            this.lblBillableValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnLoad
+            // 
+            this.BtnLoad.Location = new System.Drawing.Point(342, 206);
+            this.BtnLoad.Name = "BtnLoad";
+            this.BtnLoad.Size = new System.Drawing.Size(128, 35);
+            this.BtnLoad.TabIndex = 0;
+            this.BtnLoad.Text = "Load";
+            this.BtnLoad.UseVisualStyleBackColor = true;
+            this.BtnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(12, 206);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(128, 35);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export to CSV";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Total nr of invoices:";
+            // 
+            // lblNrOfInvoices
+            // 
+            this.lblNrOfInvoices.AutoSize = true;
+            this.lblNrOfInvoices.Location = new System.Drawing.Point(140, 9);
+            this.lblNrOfInvoices.Name = "lblNrOfInvoices";
+            this.lblNrOfInvoices.Size = new System.Drawing.Size(0, 17);
+            this.lblNrOfInvoices.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Processed invoices:";
+            // 
+            // lblProcessed
+            // 
+            this.lblProcessed.AutoSize = true;
+            this.lblProcessed.Location = new System.Drawing.Point(140, 35);
+            this.lblProcessed.Name = "lblProcessed";
+            this.lblProcessed.Size = new System.Drawing.Size(0, 17);
+            this.lblProcessed.TabIndex = 5;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 177);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(458, 23);
+            this.progressBar.TabIndex = 6;
+            // 
+            // lblBillable
+            // 
+            this.lblBillable.AutoSize = true;
+            this.lblBillable.Location = new System.Drawing.Point(9, 63);
+            this.lblBillable.Name = "lblBillable";
+            this.lblBillable.Size = new System.Drawing.Size(93, 17);
+            this.lblBillable.TabIndex = 7;
+            this.lblBillable.Text = "Total Billable:";
+            // 
+            // lblBillableValue
+            // 
+            this.lblBillableValue.AutoSize = true;
+            this.lblBillableValue.Location = new System.Drawing.Point(140, 63);
+            this.lblBillableValue.Name = "lblBillableValue";
+            this.lblBillableValue.Size = new System.Drawing.Size(0, 17);
+            this.lblBillableValue.TabIndex = 8;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 253);
+            this.Controls.Add(this.lblBillableValue);
+            this.Controls.Add(this.lblBillable);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lblProcessed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNrOfInvoices);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.BtnLoad);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(500, 300);
+            this.MinimumSize = new System.Drawing.Size(500, 300);
+            this.Name = "Main";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Billing Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLoad;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNrOfInvoices;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblProcessed;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblBillable;
+        private System.Windows.Forms.Label lblBillableValue;
+    }
+}
+
---
+++ Mobile1BillingReader/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace Mobile1BillingReader
+{
+    public partial class Main : Form
+    {
+        private StringBuilder _invoiceList;
+        private int _progres;
+        private decimal _billingvalue;
+        private int _processed;
+
+        public Main()
+        {
+            InitializeComponent();
+            if (_invoiceList == null) btnExport.Visible = false;
+
+            progressBar.Visible = false;
+        }
+
+        private async void BtnLoad_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = @"All Files (*.pdf)|*.pdf",
+                FilterIndex = 1,
+                Multiselect = true
+            };
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+            var arrAllFiles = openFileDialog.FileNames; //used when Multiselect = true
+
+            if (!arrAllFiles.Any()) return;
+            lblNrOfInvoices.Text = arrAllFiles.Length.ToString();
+            _progres = 100 / arrAllFiles.Length;
+            _billingvalue = 0;
+            _processed = 0;
+            progressBar.Visible = true;
+
+            _invoiceList = new StringBuilder();
+            foreach (var item in arrAllFiles)
+                try
+                {
+                    await ReadResult(await PdfText(item)).ConfigureAwait(false);
+                    if (progressBar.InvokeRequired)
+                        progressBar.Invoke(progressBar.Value + _progres > 100
+                            ? delegate { progressBar.Value = _progres; }
+                        : new MethodInvoker(delegate { progressBar.Value += _progres; }));
+                    else
+                        progressBar.Value = progressBar.Value + 1;
+
+                    if (lblProcessed.InvokeRequired)
+                        lblProcessed.Invoke(new MethodInvoker(delegate
+                        {
+                            lblProcessed.Text = (_processed = _processed + 1).ToString();
+                        }));
+                    else
+                        lblProcessed.Text = (_processed + 1).ToString();
+
+                    await Task.Delay(50).ConfigureAwait(false);
+                }
+                catch (Exception exception)
+                {
+                    // ReSharper disable once LocalizableElement
+                    MessageBox.Show(
+                        @"Something must have gone wrong, please contact your deveoper" + "\n\n" + exception,
+                        @"Oeps!!!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            if (!string.IsNullOrEmpty(_invoiceList.ToString()))
+                if (btnExport.InvokeRequired)
+                    btnExport.Invoke(new MethodInvoker(delegate { btnExport.Visible = true; }));
+                else
+                    btnExport.Visible = true;
+
+            if (progressBar.InvokeRequired)
+                progressBar.Invoke(new MethodInvoker(delegate { progressBar.Visible = false; }));
+            else
+                progressBar.Visible = false;
+        }
+
+        private async Task<string> PdfText(string path)
+        {
+            var reader = new PdfReader(path);
+            var text = string.Empty;
+            await Task.Run(() =>
+            {
+                for (var page = 1; page <= reader.NumberOfPages; page++)
+                    text += PdfTextExtractor.GetTextFromPage(reader, page,
+                        new SimpleTextExtractionStrategy());
+
+                reader.Close();
+            }).ConfigureAwait(false);
+
+            return text;
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog()
+            {
+                Filter = @"CSV files (*.csv)|*.csv",
+                FilterIndex = 1
+            };
+            if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
+            var filename = saveFileDialog.FileName;
+            try
+            {
+                File.WriteAllText(filename, _invoiceList.ToString());
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private async Task ReadResult(string result)
+        {
+            await Task.Run(() =>
+            {
+                var lines = Regex.Split(result, "\n");
+
+                var invoiceNr = lines[32].Trim();
+                var invoicedate = lines[33].Trim();
+                var cellPhoneNr = lines[31].Trim();
+                cellPhoneNr = cellPhoneNr.Replace(" ", "");
+
+                var startline = 0;
+                for (var i = 1; i < lines.Length; i++)
+                {
+                    if (lines[i].StartsWith("DATE TRANSACTION AMOUNT"))
+                    {
+                        startline = i + 1;
+                    }
+                }
+                for (var line = 1; line < lines.Length; line++)
+                {
+                    if (lines[line].StartsWith("TOTAL EXCLUDING VAT")) break;
+
+                    if (line < startline) continue;
+                    var itemresult = lines[line];
+                    if (itemresult == "- EAGLE EYE - 13.16 13.16")
+                    {
+                        var date = string.Empty;
+                        var item = lines[line].Substring(0, 19);
+                        var value = lines[line].Substring(20, lines[line].Length - 20);
+                        value = value.Trim();
+                        _invoiceList.AppendLine($"{invoiceNr},{invoicedate},{cellPhoneNr},{date},{item},{value}");
+                        _billingvalue = _billingvalue + decimal.Parse(value.Replace(".", ","));
+                        if (lblBillableValue.InvokeRequired)
+                            lblBillableValue.Invoke(new MethodInvoker(delegate
+                            {
+                                lblBillableValue.Text = _billingvalue.ToString(CultureInfo.InvariantCulture);
+                            }));
+                        else
+                            lblBillableValue.Text = _billingvalue.ToString(CultureInfo.InvariantCulture);
+                    }
+                    else
+                    {
+                        var tempDate = lines[line].Substring(0, 10);
+                        var date = string.Empty;
+                        // ReSharper disable once UnusedVariable
+                        if (DateTime.TryParse(tempDate, out var dt))
+                        {
+                            date = tempDate;
+                            lines[line] = lines[line].Remove(0, 10);
+                        }
+
+                        lines[line] = lines[line].TrimStart(' ');
+                        lines[line] = lines[line].TrimStart(' ');
+                        var item = Regex.Replace(lines[line], @"[\d-,.]", string.Empty);
+                        item = item.Trim();
+                        var value = lines[line].Replace(item, "");
+                        value = value.Trim();
+                        _invoiceList.AppendLine($"{invoiceNr},{invoicedate},{cellPhoneNr},{date},{item},{value}");
+                        _billingvalue = _billingvalue + decimal.Parse(value.Replace(".", ","));
+                        if (lblBillableValue.InvokeRequired)
+                            lblBillableValue.Invoke(new MethodInvoker(delegate
+                            {
+                                lblBillableValue.Text = _billingvalue.ToString(CultureInfo.InvariantCulture);
+                            }));
+                        else
+                            lblBillableValue.Text = _billingvalue.ToString(CultureInfo.InvariantCulture);
+                    }
+                }
+            }).ConfigureAwait(false);
+        }
+    }
+}
